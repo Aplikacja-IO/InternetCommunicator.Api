a42ec9ab9a48042e03189c1814bb04b5b1cdd88e
nelumboo <tk.kielkowski@gmail.com>
2021-11-28 17:58:06 +0100

    Add new project, add nuget packages, add Models, add gitignore

---
+++ InternetCommunicator.Domain/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace InternetCommunicator.Domain.Models
+{
+    public partial class Image
+    {
+        public int ComponentId { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual Component Component { get; set; }
+    }
+}
