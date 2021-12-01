7d56e386fb5b394befa9c8462977b4132a3b8443
nelumboo <tk.kielkowski@gmail.com>
2021-12-01 18:48:32 +0100

    Update after gitignore modification

---
+++ InternetCommunicator.Domain/Models/RegisterUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace InternetCommunicator.Domain.Models
+{
+    public partial class RegisterUser
+    {
+        public RegisterUser()
+        {
+            CompanyUserOwners = new HashSet<CompanyUser>();
+            ComponentSubscribers = new HashSet<ComponentSubscriber>();
+            Components = new HashSet<Component>();
+            GroupMemberships = new HashSet<GroupMembership>();
+            Groups = new HashSet<Group>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public byte[] UserPassword { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public virtual CompanyUser CompanyUserUser { get; set; }
+        public virtual ICollection<CompanyUser> CompanyUserOwners { get; set; }
+        public virtual ICollection<ComponentSubscriber> ComponentSubscribers { get; set; }
+        public virtual ICollection<Component> Components { get; set; }
+        public virtual ICollection<GroupMembership> GroupMemberships { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
