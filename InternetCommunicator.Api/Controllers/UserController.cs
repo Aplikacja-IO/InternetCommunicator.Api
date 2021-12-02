6ee64c11dcaa62ddf0b6953ef8f5d4df30eea1e4
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2021-12-02 11:59:36 +0100

    rest api user endpoints

---
+++ InternetCommunicator.Api/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using InternetCommunicator.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetCommunicator.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public RegisterUser GetUserById(int id)
+        {
+
+        }
+        public IEnumerable<RegisterUser> GetAllUsers()
+        {
+
+        }
+    }
+}
