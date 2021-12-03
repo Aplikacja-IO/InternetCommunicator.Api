153afe6b6cf8f6fd0984cc331223b9f80981df43
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2021-12-03 17:20:36 +0100

    metody get, post, delete dla registerUser i CompanyUser

---
+++ InternetCommunicator.Api/Controllers/CompanyUserController.cs
@@ -0,0 +1,52 @@
+﻿using InternetCommunicator.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetCommunicator.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyUserController : ControllerBase
+    {
+        public tmpDb database { get; set; }
+        public CompanyUserController()
+        {
+            database = tmpDb.GetInstance();
+
+            tmpDb.SetNumerOfUsersTo(10);
+            tmpDb.SetPercenOfCompanyUsersTo(50);
+        }
+
+        [HttpGet("{id}")]
+        public CompanyUser GetUserById(int id)
+        {
+            var allUsers = database.GetAllCompanyUsers();
+            var user = allUsers.Where(user => user.UserId == id);
+
+            return user.FirstOrDefault();
+        }
+        [HttpGet]
+        public IEnumerable<CompanyUser> GetAllCompanyUsers()
+        {
+            return database.GetAllCompanyUsers();
+        }
+        [HttpDelete]
+        public bool DeleteCompanyUserById(int id)
+        {
+            return database.RemoveUserById(id);
+        }
+        [HttpPost]
+        public bool PostCompanyUser(string firstName, string lastName, string login, string password, int ownerId)
+        {
+            var allUsers = database.GetAllUsers();
+            var owner = allUsers.Where(user => user.UserId == ownerId).FirstOrDefault();
+            var bytePassword = Encoding.ASCII.GetBytes(password);
+            return database.CreateNewUser(firstName, lastName, login, bytePassword, owner);
+        }
+    }
+}
---
+++ InternetCommunicator.Api/Controllers/RegisterUserController.cs
@@ -0,0 +1,54 @@
+﻿using InternetCommunicator.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetCommunicator.Api.Controllers
+{    
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class RegisterUserController : ControllerBase
+    {
+        public tmpDb database { get; set; }
+        public RegisterUserController()
+        {
+            database = tmpDb.GetInstance();
+
+            tmpDb.SetNumerOfUsersTo(10);
+            tmpDb.SetPercenOfCompanyUsersTo(50);
+        }
+
+        [HttpGet("{id}")]
+        public RegisterUser GetUserById(int id)
+        {
+            var allUsers = database.GetAllUsers();
+            var user = allUsers.Where(user => user.UserId == id);
+
+            return user.FirstOrDefault();
+        }
+
+        [HttpGet]
+        public IEnumerable<RegisterUser> GetAllUsers()
+        {
+            return database.GetAllUsers();
+        }
+
+        [HttpDelete] 
+        public bool DeleteRegisterUser(int id)
+        {
+            return database.RemoveUserById(id);
+        }
+
+        [HttpPost]
+        public bool PostRegisterUser(string login, string password)
+        {
+            var bytePassword = Encoding.ASCII.GetBytes(password);
+            return database.CreateNewUser(login, bytePassword);
+        }
+    }
+}
