248670ebfb87caec27d1c236ded33649362d9bc3
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2021-12-11 19:47:29 +0100

    rejestracja api

---
+++ InternetCommunicator.Api/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using InternetCommunicator.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InternetCommunicator.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly CommunicatorDBContext _context;
+
+        public RegistrationController(CommunicatorDBContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<RegisterUser>> PostRegisterUser(string login, string password)
+        {
+            var userService = new UserServices(_context);
+            var newUser = await userService.CreateNewUser(login, password);
+            if (newUser == null) return BadRequest("Blad podczas tworzenia uzytkownika");
+            return newUser;
+        }
+    }
+}
