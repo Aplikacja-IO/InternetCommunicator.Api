8e91f27fb714cc3b0498abfcb229304188a8f637
nelumboo <tk.kielkowski@gmail.com>
2021-12-19 21:27:16 +0100

    GroupDto

---
+++ InternetCommunicator.Application/Dto/Group/GroupDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetCommunicator.Application.Dto.Group
+{
+    public class GroupDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentGroupId { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
