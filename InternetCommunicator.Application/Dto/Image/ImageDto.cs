7cd06d8646e5ef370fcab35f27bdb68c683cd5eb
nelumboo <tk.kielkowski@gmail.com>
2022-01-26 22:32:11 +0100

    TDD Added ImageDto

---
+++ InternetCommunicator.Application/Dto/Image/ImageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace InternetCommunicator.Application.Dto.Image
+{
+    public class ImageDto
+    {
+        public int ComponentId { get; set; }
+        public string ImageUrl { get; set; }
+        public byte[] ByteArray { get; set; }
+    }
+}
