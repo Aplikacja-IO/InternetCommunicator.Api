30f009ccd315dad2da465e80ec398e38e2518b31
nelumboo <tk.kielkowski@gmail.com>
2022-01-26 18:36:45 +0100

    TDD Added FileRepository class

---
+++ InternetCommunicator.Infrastructure/Repository/File/FileRepository.cs
@@ -0,0 +1,12 @@
+﻿using InternetCommunicator.Domain.Models;
+using InternetCommunicator.Infrastructure.Context;
+
+namespace InternetCommunicator.Infrastructure.Repository
+{
+    public class FileRepository : BaseRepository<Image>, IFileRepository
+    {
+        public FileRepository(CommunicatorDbContext context)
+            : base(context)
+        { }
+    }
+}
