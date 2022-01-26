4a75938e52bc9910ed12170ae94f9b5588287eca
nelumboo <tk.kielkowski@gmail.com>
2022-01-26 18:47:03 +0100

    TDD Added GetImageQuery

---
+++ InternetCommunicator.Application/Feature/Image/GetImageQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+
+namespace InternetCommunicator.Application.Feature.Image
+{
+    class GetImageQuery : IRequest<Domain.Models.Image>
+    {
+        public int Id { get; set; }
+
+        public GetImageQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
+
+
